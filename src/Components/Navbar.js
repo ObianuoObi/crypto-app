90bff5b061af8bb5d6d3dc772a3d1727a404c660
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-05-02 20:30:00 +0100

    Navbar styled via Navbar.css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,37 @@
 import React from 'react'
 import {FaBars, FaTimes} from "react-icons/fa"
+import "./Navbar.css"
+
+
+
 const Navbar = () => {
+
+
+
   return (
-    <div className='header'>
-    <div className='container'>
-<h1>Cry<span className='primary'>Pto</span></h1>
-<ul className='nav-menu'>
-<li>
-    <a href='/'>Home</a>
-</li>
-<li>
-    <a href='/'>Featured</a>
-</li>
-<li>
-    <a href='/'>Earn</a>
-</li>
-<li>
-    <a href='/'>Contact</a>
-</li>
-</ul>
+    
+<div className='header'>
+   <div className='container'>
+      <h1>Cry<span className='primary'>Pto</span></h1>
+         <ul className='nav-menu'>
+          <li>
+             <a href='/'>Home</a>
+         </li>
+            <li>
+              <a href='/'>Featured</a>
+               </li>
+            <li>
+               <a href='/'>Earn</a>
+                </li>
+              <li>
+                 <a href='/'>Contact</a>
+             </li>
+         </ul>
 <div className='btn-group'>
     <button className='btn'> Connect Wallet</button>
 </div>
 <div className='hamburger'>
-<FaBars/>
+<FaBars size={20} style={color}/>
 </div>
     </div>
     </div>
