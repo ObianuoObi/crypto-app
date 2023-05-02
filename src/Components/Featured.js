529d0f8a1e61586361ee65011ad7603aa65fa46d
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-05-02 23:17:56 +0100

    Featured.js skeleton created

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Featured.css'
+import {FiArrowRight, FiArrowDown} from 'react-icons/fi' 
 
 const Featured = () => {
+
+    
+
   return (
-    <div>
-        
+    <div className='featured'>
+        <div className='container'>
+            {/* Left */}
+            <div className='left'>
+                <h2>Explore top Cryptocurrencies like Bitcoin, Ehereum, Shiba Inu</h2>
+                <p>See all available assets: Cryptocurrencies and NFT's</p>
+                <button className='btn'> See More Coins</button>
+            </div>
+            {/* Right */}
+            <div className='right'>
+                <div className='top'>
+                    <img src= {} alt=''/>
+                </div>
+                <div>
+                <h5>Bitcoin</h5>
+                <p>£30,000</p>
+                </div>
+                <span><FiArrowRight/>2.5%</span>
+            </div>
+
+        </div>
     </div>
   )
 }
