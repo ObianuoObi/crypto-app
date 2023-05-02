173751f650b51857613c516afc75fcae58b10fe5
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-05-02 22:30:31 +0100

    Hero component styling completed

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
             <div className='left'>
                 <p>Buy & Sell Crypto 24/7 with your retirement account</p>
                 <h1>Invest in Cryptocurrency with Your IRA</h1>
-                <p>Buy, Sell & store hundreds of cryptocurrencies</p>
+                <p>Buy, Sell & Store hundreds of cryptocurrencies</p>
                 <div className='input-container'>
                     <input type='email' placeholder='Enter your email'/>
                     <button className='btn'>Learn More</button> 
@@ -22,7 +22,6 @@ const Hero = () => {
             <div className='right'>
                 <div className='img-container'>
                     <img src={Crypto} alt=''/>
-
                 </div>
                 </div>
         </div>
